0642ddfd663bfeb8ac7a38a01ebf431d8ddde4f3
linyuanbin <linyuanbin@qianxin.com>
2020-04-01 20:25:27 +0800

    add dev args

--- printer/html.go
+++ printer/html.go
@@ -3,15 +3,17 @@ package printer
 import (
 	"fmt"
 	"github.com/linyuanbin/fmt-conv/xlog"
+	"github.com/mafredri/cdp/protocol/emulation"
 )
 
 // NewHTMLPrinter returns a Printer which
 // is able to convert an HTML file to PDF.
-func NewHTMLPrinter(logger xlog.Logger, fpath string, opts ChromePrinterOptions) Printer {
+func NewHTMLPrinter(logger xlog.Logger, fpath string, opts ChromePrinterOptions, devArgs *emulation.SetDeviceMetricsOverrideArgs) Printer {
 	URL := fmt.Sprintf("file://%s", fpath)
 	return chromePrinter{
-		logger: logger,
-		url:    URL,
-		opts:   opts,
+		logger:  logger,
+		url:     URL,
+		opts:    opts,
+		devArgs: devArgs,
 	}
 }
